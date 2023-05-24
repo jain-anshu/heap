e3a7c41bbcd9453e3f7f7ca6c873904e14480bed
jain-anshu <anshuj@gmail.com>
2023-05-24 13:39:10 -0700

    Refactors

--- lib/heap.rb
+++ lib/heap.rb
@@ -1,59 +1,62 @@
+# frozen_string_literal: true
+
 class MinHeap
-    def initialize
-      @heap = []
-    end
+  def initialize
+    @heap = []
+  end
 
-    def peek
-      @heap[0]
-    end
+  def peek
+    @heap[0]
+  end
 
-    def insert(val)
-        @heap << val
-        heapify_up(@heap.length - 1)
-    end
-    
-    def get_min
-        return nil if @heap.empty?
-        
-        swap(0, @heap.length - 1)
-        val = @heap.pop()
-        heapify_down(0)
-        val
-    end
+  def insert(val)
+    @heap << val
+    heapify_up(last_ind)
+  end
 
-    def size
-        @heap.length - 1
-    end
+  def get_min
+    return nil if @heap.empty?
 
-    private
-    def heapify_up(ind)
-        parent_ind = (ind - 1) / 2
+    swap(0, last_ind)
+    val = @heap.pop
+    heapify_down(0)
+    val
+  end
 
-        if ind > 0 && @heap[parent_ind] > @heap[ind]
-            swap(ind, parent_ind)
-            heapify_up(parent_ind)
-        end    
-    end
+  private
 
-    def heapify_down(ind)
-        l_child_ind = ind * 2 + 1
-        r_child_ind = ind * 2 + 2
-        smallest = ind
-        if l_child_ind < @heap.length && @heap[l_child_ind] < @heap[smallest]
-            smallest = l_child_ind
-        end
-        
-        if r_child_ind < @heap.length && @heap[r_child_ind] < @heap[smallest]
-            smallest = r_child_ind
-        end
+  def heapify_up(ind)
+    parent_ind = (ind - 1) / 2
 
-        if smallest != ind
-            swap(ind, smallest)
-            heapify_down(smallest)
-        end
-    end
+    return unless ind.positive? && compare(ind, parent_ind)
 
-    def swap(i, j)
-        @heap[i], @heap[j] = @heap[j], @heap[i]
-    end
-end                                                                   
+    swap(ind, parent_ind)
+    heapify_up(parent_ind)
+  end
+
+  def heapify_down(ind)
+    l_child_ind = ind * 2 + 1
+    r_child_ind = ind * 2 + 2
+    smallest = ind
+    smallest = l_child_ind if compare(l_child_ind, smallest)
+
+    smallest = r_child_ind if compare(r_child_ind, smallest)
+
+    return unless smallest != ind
+
+    swap(ind, smallest)
+    heapify_down(smallest)
+  end
+
+  def compare(i, j)
+    i < @heap.length && @heap[i] < @heap[j]
+  end
+
+  def swap(i, j)
+    @heap[i], @heap[j] = @heap[j], @heap[i]
+  end
+
+  def last_ind
+    @heap.length - 1
+  end
+end
--- spec/heap_spec.rb
+++ spec/heap_spec.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 require './lib/heap'
 
-describe "MinHeap" do 
-    describe "#peek" do
-      it "returns the smallest number in the MinHeap" do
-        mh = MinHeap.new()
-        mh.insert(3)
-        mh.insert(5)
-        mh.insert(2)
-        expect(mh.peek).to eql(2)
-      end
+describe 'MinHeap' do
+  describe '#peek' do
+    it 'returns the smallest number in the MinHeap' do
+      mh = MinHeap.new
+      mh.insert(3)
+      mh.insert(5)
+      mh.insert(2)
+      expect(mh.peek).to eql(2)
     end
+  end
 end
